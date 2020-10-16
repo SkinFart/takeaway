a33f08ffe6e3d1b29f4bbbbc9c7d13ccdddec8d7
William <st17024@howick.school.nz>
2020-10-16 14:28:38 +1300

    commit

--- main.py
+++ main.py
@@ -49,9 +49,40 @@ def phone():
         except ValueError:
             print("Please enter a valid input")
 
+def order():
+    #s=[] #order list
+    z={} #test dictionary
+    t=input("Would you like to make an order? If not please leave blank. ")
+    while t != "":
+        try:
+            item=int(input("Please make a selection 1-12 of the menu (0 to finish order): "))
+            if item >=1 and item <=12:
+                quantity=int(input("How many: "))
+                if quantity > 5:
+                    print("Max order limit is 5. ")
+                elif quantity == 0:
+                    print("Item not added. ")
+                else:
+                    f='item'+str(item) #creates item search term
+                    d=menu[f] #sets the order item to a variable
+                    n=d['item'] #pulls item name from dictionary
+                    #s.append(n) #adds to order list using the item name
+                    if n in z:
+                        z[n]+=1
+                    else:
+                        z[n]=quantity #sends the order and quantiy to a dictionary
+            elif item == 0:
+                return z
+        except ValueError:
+            print("Not a valid input.")
+
+t=""
+
 #main program
 greeting()
 customer=customer_name()
 delivery_address=address()
 contact_number=phone()
 display()
+a=order()
+print(a)
